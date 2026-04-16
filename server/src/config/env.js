732c27c6ad4ff2558413b010cd08c4b8367a294d
cesarramos0 <cesar.ramos.moron@alu.ceacfp.es>
2026-04-16 12:50:32 +0200

    fix: chage files whit similar names

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,13 +1,5 @@
 require('dotenv').config();
 
-const VARIABLES_REQUERIDAS = ['PORT'];
-
-VARIABLES_REQUERIDAS.forEach((variable) => {
-  if (!process.env[variable]) {
-    throw new Error(`Variable de entorno requerida no definida: ${variable}`);
-  }
-});
-
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
 };
